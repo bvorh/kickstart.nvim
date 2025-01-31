ff9074797db9a0d860f0e7ade7a99bae0a221289
Bernhard Vorhofer <bernhard.vorhofer@robco.de>
2025-01-31 10:19:32 +0100

    Add git-worktree, customize mappings

---
+++ lua/custom/plugins/git-worktree.lua
@@ -0,0 +1,28 @@
+return {
+  'polarmutex/git-worktree.nvim',
+  version = '^2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local Hooks = require 'git-worktree.hooks'
+    local config = require 'git-worktree.config'
+    local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
+
+    Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
+      vim.notify('Moved from ' .. prev_path .. ' to ' .. path)
+      update_on_switch(path, prev_path)
+    end)
+
+    Hooks.register(Hooks.type.DELETE, function()
+      vim.cmd(config.update_on_change_command)
+    end)
+
+    local telescope = require 'telescope'
+    telescope.load_extension 'git_worktree'
+    vim.keymap.set('n', '<leader>gwl', function()
+      telescope.extensions.git_worktree.git_worktree()
+    end, { desc = 'List git worktrees' })
+    vim.keymap.set('n', '<leader>gwc', function()
+      telescope.extensions.git_worktree.create_git_worktree()
+    end, { desc = 'Create git worktree' })
+  end,
+}
