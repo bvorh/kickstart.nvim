af653a85176788a5d7e2a854205ea8657c23f59f
Bernhard Vorhofer <bernhard.vorhofer@robco.de>
2026-01-05 10:41:02 +0100

    Add diffview bindings

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,4 +1,8 @@
 return {
   'sindrets/diffview.nvim',
-  config = function() end,
+  config = function()
+    local diffview = require 'diffview'
+    vim.keymap.set('n', '<leader>gdo', diffview.open, { desc = '[G]it [D]iffView [o]pen' })
+    vim.keymap.set('n', '<leader>gdc', diffview.close, { desc = '[G]it [D]iffView [c]lose' })
+  end,
 }
